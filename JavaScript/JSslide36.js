0ec9b747c1eba3019300ff5f431b90cbea09b19e
daothanhxuan <daoxuann6@gmail.com>
2023-10-24 14:34:02 +0700

    JSslide36.js

---
+++ JavaScript/JSslide36.js
@@ -0,0 +1,48 @@
+function validate() {
+    var u = document.getElementById("username").value;
+    var p1 = document.getElementById("password").value;
+    var p2 = document.getElementById("password-repeat").value;
+    var p3 = document.getElementById("mail").value;
+    var p4 = document.getElementById("tel").value;
+    var p5 = document.getElementById("answer").value;
+    
+    if(u== "") {
+    alert("Vui lòng nhập tên!");
+    return false;
+    }
+    if(p1 == "") {
+    alert("Vui lòng nhập mật khẩu!");
+    return false;
+    }
+    if(p2 == "") {
+    alert("Vui lòng xác minh mật khẩu!");
+    return false;
+    }
+    if(p2 != p3){
+        alert("Mật khẩu không đúng, vui lòng nhập lại!");
+        return false;
+    }
+    
+    if(p3== "") {
+        alert("Vui lòng nhập mail!");
+        return false;
+    }
+    if(p4 == "") {
+        alert("Vui lòng nhập số điện thoại!");
+        return false;
+    }
+    if(p5 == "") {
+        alert("Vui lòng nhập câu trả lời!");
+        return false;
+    }
+
+    alert("Vui lòng điền đầy đủ thông tin!");
+    
+    return true;
+
+    
+
+    }
+
+    
+    
