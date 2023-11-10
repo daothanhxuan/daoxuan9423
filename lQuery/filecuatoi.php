2309bf96fac056ce047f3201112777292fce52fa
daothanhxuan <daoxuann6@gmail.com>
2023-11-10 11:09:24 +0700

    baitapphp10/11/2023

---
+++ lQuery/filecuatoi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>    
+    <body>
+        <?php
+            echo "MY FIRST PHP FILE";
+            echo "<h3>Hello World!</h3>";
+            echo "<select>";
+            echo "<option>1</option>";
+            echo "<option>2</option>";
+            echo "</select>";
+        ?>
+    </body>
+</html>        
